2dcbc558d71255e926b9e6d24ec30e9365ab264d
tracer-ics2016 <tracer@njuics.org>
2016-09-04 13:05:54 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  13:05:54 up  2:48,  1 user,  load average: 1.15, 1.15, 1.14 3eabb62a137093211c2a5aa42d3331a1e71de75

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -14,18 +14,19 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef struct {
+typedef union {
 	union {
 		uint8_t _8[2];
 		uint16_t _16;
 		uint32_t _32;
 	} gpr[8];
 
-	/* Do NOT change the order of the GPRs' definitions. */
+	struct {
+		/* Do NOT change the order of the GPRs' definitions. */
+		uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 
-	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
-	swaddr_t eip;
+		swaddr_t eip;
+	};
 
 } CPU_state;
 
