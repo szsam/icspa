a259a8a37c7caf99ecf17a6a5df888a0753cd8c4
tracer-ics2016 <tracer@njuics.org>
2016-09-06 16:31:02 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  16:31:02 up  1:16,  1 user,  load average: 1.02, 1.08, 1.12 add88508e0e9ef5c7c84994f8b66311be91e2591

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -1,4 +1,5 @@
 #include "nemu.h"
+#include <stdlib.h>
 
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
@@ -128,6 +129,67 @@ static bool make_token(char *e) {
 	return true; 
 }
 
+bool check_parentheses(int p, int q) {
+	int count = 0;
+	bool once_empty = false;
+
+	for (int ix = p; ix <= q; ix++) {
+		if (tokens[ix].type == '(')
+			++count;
+		else if (tokens[ix].type == ')') {
+			--count;
+			if (count < 0)
+				panic("Parentheses do not match");
+			else if (count == 0)
+				once_empty = true;
+		}
+
+	}
+
+	if (count > 0)
+		panic("Parentheses do not match");
+	return tokens[p].type == '(' && tokens[q].type == ')' && !once_empty;	
+}
+
+//return the position of dominant operator in the token expression;
+int dominant_operator(int p, int q) {
+	panic("Waiting for implemention");
+}
+
+uint32_t eval(int p, int q) {
+	if(p > q) {
+		/* Bad expression */
+		assert(0);
+	}
+	else if(p == q) { 
+		/* Single token.
+		 * For now this token should be a number. 
+		 * Return the value of the number.
+		 */ 
+		return atoi(tokens[p].str);
+	}
+	else if(check_parentheses(p, q) == true) {
+		/* The expression is surrounded by a matched pair of parentheses. 
+		 * If that is the case, just throw away the parentheses.
+		 */
+		return eval(p + 1, q - 1); 
+	}
+	else {
+		//op = the position of dominant operator in the token expression;
+		int op = dominant_operator(p, q);
+		uint32_t val1 = eval(p, op - 1);
+		uint32_t val2 = eval(op + 1, q);
+
+		switch(tokens[op].type) {
+			case '+': return val1 + val2;
+			case '-': return val1 - val2;
+			case '*': return val1 * val2; 
+			case '/': return val1 / val2; 
+			default: assert(0);
+		}
+	}
+}
+
 uint32_t expr(char *e, bool *success) {
 	if(!make_token(e)) {
 		*success = false;
@@ -135,7 +197,8 @@ uint32_t expr(char *e, bool *success) {
 	}
 
 	/* TODO: Insert codes to evaluate the expression. */
-	panic("please implement me");
-	return 0;
+	//panic("please implement me");
+	
+	return eval(0, nr_token-1);
 }
 
