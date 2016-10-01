b383db7f943205dc8488456163ebc4347f6a8069
tracer-ics2016 <tracer@njuics.org>
2016-10-01 16:45:01 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  16:45:01 up  6:19,  1 user,  load average: 1.27, 1.30, 1.31 d16dbb68447ea283b56712a3b8939d41bc55e5e7

--- nemu/src/cpu/exec/control/ret.c
+++ nemu/src/cpu/exec/control/ret.c
@@ -1,8 +1,11 @@
 #include "cpu/helper.h"
+#include "cpu/exec/helper.h"
 
 make_helper(ret) {
 	swaddr_t ret_addr = swaddr_read(cpu.esp, 4);
 	cpu.esp += 4;
 	cpu.eip = ret_addr;
+
+	print_asm_template1();
 	return 0;
 }
