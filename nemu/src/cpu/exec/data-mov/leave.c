0028bbcb421a61e24f98672d62080fa54724b47c
tracer-ics2016 <tracer@njuics.org>
2016-10-11 23:03:27 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  23:03:27 up  3:40,  1 user,  load average: 1.15, 1.20, 1.18 e131ae4095aae9d3e18751e7f6babfd9c7a48eb

---
+++ nemu/src/cpu/exec/data-mov/leave.c
@@ -0,0 +1,11 @@
+#include "cpu/helper.h"
+#include "cpu/exec/helper.h"
+
+make_helper(leave) {
+	cpu.esp = cpu.ebp;
+	cpu.ebp = swaddr_read(cpu.esp, 4);
+	cpu.esp += 4;
+
+	print_asm("leave");
+	return 1;
+}
