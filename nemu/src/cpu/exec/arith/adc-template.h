ffd8c404456ef150352556c50efea8b923f3bb74
tracer-ics2016 <tracer@njuics.org>
2016-10-12 21:25:14 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  21:25:14 up 57 min,  1 user,  load average: 1.05, 1.10, 1.13 81073a0684f5882d9560f11e6f87a79b82b2423e

---
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -0,0 +1,30 @@
+#include "cpu/exec/template-start.h"
+#include "cpu/exec/flags.h"
+
+#define instr adc
+
+static void do_execute() {
+	DATA_TYPE result = op_src->val + op_dest->val + cpu.CF;
+
+	cpu.SF = MSB(result);
+	cpu.ZF = (result == 0);
+	cpu.PF = EVEN_PARITY(result);
+    cpu.CF = !UADD_OK(op_dest->val, op_src->val) || !UADD_OK(op_dest->val+op_src->val, 1);
+    cpu.OF = !TADD_OK(op_dest->val, op_src->val);	// this is buggy!
+
+	OPERAND_W(op_dest, result);
+
+	print_asm_template2();
+}
+
+make_instr_helper(i2r)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+
+
+#include "cpu/exec/template-end.h"
---
+++ nemu/src/cpu/exec/arith/adc.h
@@ -0,0 +1,15 @@
+#ifndef __ADC_H__
+#define __ADC_H__
+
+make_helper(adc_i2r_b);
+make_helper(adc_i2rm_b);
+make_helper(adc_r2rm_b);
+make_helper(adc_rm2r_b);
+
+make_helper(adc_i2r_v);
+make_helper(adc_i2rm_v);
+make_helper(adc_r2rm_v);
+make_helper(adc_rm2r_v);
+make_helper(adc_si2rm_v);
+
+#endif
