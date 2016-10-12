44bf6c2660802a7988513e3751b58e8be1957cc6
tracer-ics2016 <tracer@njuics.org>
2016-10-12 21:08:02 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  21:08:02 up 40 min,  1 user,  load average: 1.15, 1.09, 1.06 bf971241e095a01930a018d5be3bb0c4c14cb118

--- nemu/src/cpu/exec/control/jcc.h
+++ nemu/src/cpu/exec/control/jcc.h
@@ -4,6 +4,7 @@
 make_helper(je_si_b);
 make_helper(ja_si_b);
 make_helper(jae_si_b);
+make_helper(jb_si_b);
 make_helper(jbe_si_b);
 make_helper(jg_si_b);
 make_helper(jge_si_b);
@@ -13,6 +14,7 @@ make_helper(jle_si_b);
 make_helper(je_si_l);
 make_helper(ja_si_l);
 make_helper(jae_si_l);
+make_helper(jb_si_l);
 make_helper(jbe_si_l);
 make_helper(jg_si_l);
 make_helper(jge_si_l);
