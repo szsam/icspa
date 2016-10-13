d2abcad2b3cf880e67ef14e9b8de5ccf7297ecd6
tracer-ics2016 <tracer@njuics.org>
2016-10-13 14:51:40 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  14:51:40 up  6:48,  1 user,  load average: 1.72, 1.72, 1.66 14a506bd4685ef31976cb7a78a51f630f8f95195

---
+++ nemu/src/cpu/exec/logic/setcc-template.h
@@ -0,0 +1,120 @@
+#include "cpu/exec/template-start.h"
+
+#define instr sete
+
+static void do_execute() {
+	OPERAND_W(op_src, cpu.ZF);
+	print_asm(str(instr)" %s", op_src->str);
+}
+
+make_instr_helper(si)
+
+#undef instr
+	
+
+#define instr setne
+
+static void do_execute() {
+	OPERAND_W(op_src, !cpu.ZF);
+	print_asm(str(instr)" %s", op_src->str);
+}
+
+make_instr_helper(si)
+
+#undef instr
+	
+#define instr seta
+
+static void do_execute() {
+	OPERAND_W(op_src, cpu.CF == 0 && cpu.ZF == 0);
+	print_asm(str(instr)" %s", op_src->str);
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+	
+#define instr setae
+
+static void do_execute() {
+	OPERAND_W(op_src, cpu.CF == 0);
+	print_asm(str(instr)" %s", op_src->str);
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+	
+#define instr setb
+
+static void do_execute() {
+	OPERAND_W(op_src, cpu.CF == 1);
+	print_asm(str(instr)" %s", op_src->str);
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+	
+#define instr setbe
+
+static void do_execute() {
+	OPERAND_W(op_src, cpu.CF || cpu.ZF);
+	print_asm(str(instr)" %s", op_src->str);
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+
+#define instr setg
+
+static void do_execute() {
+	OPERAND_W(op_src, !cpu.ZF && cpu.SF == cpu.OF);
+	print_asm(str(instr)" %s", op_src->str);
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+
+#define instr setge
+
+static void do_execute() {
+	OPERAND_W(op_src, cpu.SF == cpu.OF);
+	print_asm(str(instr)" %s", op_src->str);
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#define instr setl
+
+static void do_execute() {
+	OPERAND_W(op_src, cpu.SF != cpu.OF);
+	print_asm(str(instr)" %s", op_src->str);
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+
+#define instr setle
+
+static void do_execute() {
+	OPERAND_W(op_src, cpu.ZF || cpu.SF != cpu.OF);
+	print_asm(str(instr)" %s", op_src->str);
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#include "cpu/exec/template-end.h"
---
+++ nemu/src/cpu/exec/logic/setcc.h
@@ -0,0 +1,16 @@
+#ifndef __SETCC_H__
+#define __SETCC_H__
+
+make_helper(sete_si_b);
+make_helper(setne_si_b);
+make_helper(seta_si_b);
+make_helper(setae_si_b);
+make_helper(setb_si_b);
+make_helper(setbe_si_b);
+make_helper(setg_si_b);
+make_helper(setge_si_b);
+make_helper(setl_si_b);
+make_helper(setle_si_b);
+
+
+#endif
