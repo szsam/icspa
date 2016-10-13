66be0bffe037530c8e3474a5ed3a2dd54b872602
tracer-ics2016 <tracer@njuics.org>
2016-10-13 14:02:09 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  14:02:09 up  5:59,  1 user,  load average: 2.11, 1.71, 1.63 475a1d1e1f4fd629e5365e1238d998881f5738f

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -6,6 +6,7 @@
 #include "data-mov/pop.h"
 #include "data-mov/leave.h"
 #include "data-mov/cwd_cdq.h"
+#include "data-mov/movzx.h"
 
 #include "arith/dec.h"
 #include "arith/inc.h"
--- nemu/src/cpu/exec/data-mov/movzx.c
+++ nemu/src/cpu/exec/data-mov/movzx.c
@@ -8,3 +8,16 @@ make_helper(movzwl) {
 	reg_l(op_dest->reg) = op_src->val;
 	return len+1;
 }
+
+make_helper(movzbv) {
+	op_src->size = 8;
+	int len = read_ModR_M(eip, op_src, op_dest);
+
+	if (ops_decoded.is_operand_size_16) {
+		reg_w(op_dest->reg) = (uint16_t)op_src->val;
+	}
+	else {
+		reg_l(op_dest->reg) = op_src->val;
+	}
+	return len+1;
+}
