b2f7daaa8fb08ababd0e61c52dbeefe3df31c032
tracer-ics2016 <tracer@njuics.org>
2016-10-13 17:19:01 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  17:19:01 up  9:16,  1 user,  load average: 1.44, 1.45, 1.48 22b7127fbf74497eb21e59b0e37e06bfb6c53425

---
+++ nemu/src/cpu/exec/data-mov/movs-template.h
@@ -0,0 +1,14 @@
+#include "cpu/exec/template-start.h"
+
+#define instr movs
+
+make_helper(concat(movs_, SUFFIX)) {
+	DATA_TYPE data = MEM_R(cpu.esi);
+	MEM_W(cpu.edi, data);
+	int IncDec = cpu.DF ? -DATA_BYTE : DATA_BYTE;
+	cpu.esi += IncDec;
+	cpu.edi += IncDec;
+	return 1;
+}
+
+#include "cpu/exec/template-end.h"
