f32747f0194a34135e385eb7052ab3350422dd8e
tracer-ics2016 <tracer@njuics.org>
2016-10-13 15:36:45 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  15:36:45 up  7:33,  1 user,  load average: 1.35, 1.52, 1.58 6a04da1cdacd28ee1e6b0e42747a3fc9706913e2

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -3,13 +3,20 @@
 #define instr jmp
 
 static void do_execute() {
-    uint32_t offset = op_src->val;
-
-	cpu.eip += offset;
-
-	print_asm("jmp %x", cpu.eip + 1 + DATA_BYTE);
+	if (op_src->type == OP_TYPE_IMM) {
+		uint32_t offset = op_src->val;
+		cpu.eip += offset;
+		print_asm("jmp %x", cpu.eip + 1 + DATA_BYTE);
+	}
+	else {
+		cpu.eip = op_src->val;
+		print_asm("jmp *%s", op_src->str);
+	}
 }
 
 make_instr_helper(si)
+#if DATA_BYTE == 4
+make_instr_helper(rm)
+#endif
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/control/jmp.h
+++ nemu/src/cpu/exec/control/jmp.h
@@ -2,7 +2,8 @@
 #define __JMP_H__
 
 make_helper(jmp_si_b);
-make_helper(jmp_si_l);
 
+make_helper(jmp_si_l);
+make_helper(jmp_rm_l);
 
 #endif
