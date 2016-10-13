625bfdae21f307507cc536172a1ab85a5bf4771b
tracer-ics2016 <tracer@njuics.org>
2016-10-13 14:27:55 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  14:27:55 up  6:25,  1 user,  load average: 1.83, 1.70, 1.64 35af162b39b97bb79edfb1483ea7aa292fff1de

--- nemu/src/cpu/exec/data-mov/movzx.c
+++ nemu/src/cpu/exec/data-mov/movzx.c
@@ -6,6 +6,10 @@ make_helper(movzwl) {
 	op_src->size = 2;
 	int len = read_ModR_M(eip+1, op_src, op_dest);
 	reg_l(op_dest->reg) = op_src->val;
+
+	snprintf(op_dest->str, OP_STR_SIZE, "%%%s", regsl[op_dest->reg]);
+	print_asm("movzwl %s, %s", op_src->str, op_dest->str);
+
 	return len+1;
 }
 
@@ -15,9 +19,13 @@ make_helper(movzbv) {
 
 	if (ops_decoded.is_operand_size_16) {
 		reg_w(op_dest->reg) = (uint16_t)op_src->val;
+		snprintf(op_dest->str, OP_STR_SIZE, "%%%s", regsw[op_dest->reg]);
+		print_asm("movzbw %s, %s", op_src->str, op_dest->str);
 	}
 	else {
 		reg_l(op_dest->reg) = op_src->val;
+		snprintf(op_dest->str, OP_STR_SIZE, "%%%s", regsl[op_dest->reg]);
+		print_asm("movzbl %s, %s", op_src->str, op_dest->str);
 	}
 	return len+1;
 }
