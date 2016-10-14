b0f6747394de963b1429950f9f2c4352710d697c
tracer-ics2016 <tracer@njuics.org>
2016-10-14 21:46:02 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  21:46:02 up  9:07,  1 user,  load average: 1.29, 1.19, 1.16 24452d2012c7a7036ec8f9709c6606e3533bec64

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -1,19 +1,38 @@
 #include "cpu/exec/template-start.h"
 
-#define instr call
+// #define instr call
 
-static void do_execute() {
+make_helper(call_si_l) {
+	int len = decode_si_l(eip+1);
+
 	uint32_t offset = op_src->val;
 
-    cpu.esp -= DATA_BYTE;
-    swaddr_write(cpu.esp, DATA_BYTE, cpu.eip + DATA_BYTE + 1);
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, cpu.eip + 4 + 1);
 
     cpu.eip += offset; 
 
-	print_asm("call %x", cpu.eip + DATA_BYTE + 1);	
+	print_asm("call %x", cpu.eip + 4 + 1);	
 
+	return 1+len;
 }
 
-make_instr_helper(si)
+make_helper(call_rm_l) {
+	int len = decode_rm_l(eip+1);
+
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp, 4, cpu.eip + len + 1);
+
+    cpu.eip = op_src->val; 
+
+	print_asm("call *%s", op_src->str);	
+
+	return 1+len;
+}
+
+//make_instr_helper(si)
+//#if DATA_BYTE == 4
+//make_instr_helper(rm)
+//#endif
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/control/call.h
+++ nemu/src/cpu/exec/control/call.h
@@ -2,6 +2,7 @@
 #define __CALL_H__
 
 make_helper(call_si_l);
+make_helper(call_rm_l);
 
 
 #endif
