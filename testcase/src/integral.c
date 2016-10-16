d3c1d87550345845d4a71935f98d054d66582f62
tracer-ics2016 <tracer@njuics.org>
2016-10-16 10:56:22 +0800

    >  run 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  10:56:22 up 11:52,  1 user,  load average: 1.19, 1.15, 1.14 f8226e1642fe5e1dfc6d1ccd05a8968186dd1bb

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -21,34 +21,34 @@ FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
 }
 
 int main() { 
-//	FLOAT a = computeT(10, f2F(-1.0), f2F(1.0), f);
-//	FLOAT ans = f2F(0.551222);
-//
-//	nemu_assert(Fabs(a - ans) < f2F(1e-4));
+	FLOAT a = computeT(10, f2F(-1.0), f2F(1.0), f);
+	FLOAT ans = f2F(0.551222);
+
+	nemu_assert(Fabs(a - ans) < f2F(EPSILON));
 	
- 	int a = F2int(f2F(12.0));
- 	nemu_assert(a == 12);
- 	FLOAT b = F_mul_int( int2F(a), 3);
- 	nemu_assert(b == f2F(36.0));
- 
- 	nemu_assert(f2F(127.0) == int2F(127));
- 	nemu_assert(f2F(12345.0) == int2F(12345));
- 
- 	nemu_assert(Fabs(b) == b);
- 	nemu_assert(Fabs(f2F(-34.56)) == f2F(34.56));
- 
- 	nemu_assert(F_div_int(b, 9) == int2F(4));
- 
- 	FLOAT c = F_mul_F(f2F(12.1),f2F(25.3));
- 	FLOAT d = f2F(306.13);
-	nemu_assert(f2F(12.1) == 792986);
-	nemu_assert(f2F(25.3) == 1658061);
-	nemu_assert(c == 20062548);
- 	nemu_assert(Fabs(c-d) < f2F(EPSILON));
- 
- 	FLOAT aa = f2F(2.0909);
- 	FLOAT bb = F_div_F(f2F(25.3),f2F(12.1));
- 	nemu_assert(Fabs(aa-bb) < f2F(EPSILON));
+// 	int a = F2int(f2F(12.0));
+// 	nemu_assert(a == 12);
+// 	FLOAT b = F_mul_int( int2F(a), 3);
+// 	nemu_assert(b == f2F(36.0));
+// 
+// 	nemu_assert(f2F(127.0) == int2F(127));
+// 	nemu_assert(f2F(12345.0) == int2F(12345));
+// 
+// 	nemu_assert(Fabs(b) == b);
+// 	nemu_assert(Fabs(f2F(-34.56)) == f2F(34.56));
+// 
+// 	nemu_assert(F_div_int(b, 9) == int2F(4));
+// 
+// 	FLOAT c = F_mul_F(f2F(12.1),f2F(25.3));
+// 	FLOAT d = f2F(306.13);
+//	nemu_assert(f2F(12.1) == 792986);
+//	nemu_assert(f2F(25.3) == 1658061);
+//	nemu_assert(c == 20062548);
+// 	nemu_assert(Fabs(c-d) < f2F(EPSILON));
+// 
+// 	FLOAT aa = f2F(2.0909);
+// 	FLOAT bb = F_div_F(f2F(25.3),f2F(12.1));
+// 	nemu_assert(Fabs(aa-bb) < f2F(EPSILON));
 
 	return 0;
 }
