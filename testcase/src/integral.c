d593b018dfacaef34a1832d4279edf53584d8661
tracer-ics2016 <tracer@njuics.org>
2016-10-16 09:39:15 +0800

    >  run 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  09:39:15 up 10:35,  1 user,  load average: 1.23, 1.15, 1.15 365e2a7ce2397399c5ce73cdec4be09c555638a9

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -22,7 +22,7 @@ int main() {
 	FLOAT a = computeT(10, f2F(-1.0), f2F(1.0), f);
 	FLOAT ans = f2F(0.551222);
 
-	nemu_assert(Fabs(a - ans) < f2F(1e-3));
+	nemu_assert(Fabs(a - ans) < f2F(1e-4));
 	
 // 	int a = F2int(f2F(12.0));
 // 	nemu_assert(a == 12);
