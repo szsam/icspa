b0c47531faa1a97eb9406d49e8630d057e03462a
tracer-ics2016 <tracer@njuics.org>
2016-10-27 15:52:53 +0800

    >  compile kernel 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  15:52:53 up  5:42,  1 user,  load average: 1.48, 1.25, 1.18 e6300e3845c3cd5ffa18b8cfaa9add1e173c15b1

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -52,7 +52,8 @@ uint32_t loader() {
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			memset((void *)(ph->p_vaddr) + ph->p_filesz, 0, ph->p_memsz-ph->p_filesz);
+			if (ph->p_memsz > ph->p_filesz)
+				memset((void *)(ph->p_vaddr) + ph->p_filesz, 0, ph->p_memsz-ph->p_filesz);
 			cnt++;
 			set_bp();
 #ifdef IA32_PAGE
