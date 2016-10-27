4f915f96e8d4fb7ef3877000e5aacbd0a9cceac1
tracer-ics2016 <tracer@njuics.org>
2016-10-27 20:49:29 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  20:49:29 up 10:39,  1 user,  load average: 1.55, 1.69, 1.74 6f3cd08b9b01c74db14277589283191e2ea4f930

--- testcase/src/print-FLOAT.c
+++ testcase/src/print-FLOAT.c
@@ -7,22 +7,22 @@
 char buf[128];
 
 int main() {
-//	init_FLOAT_vfprintf();
-//
-//#ifdef LINUX_RT
-//	printf("%f\n", FLOAT_ARG(0x00010000));
-//	printf("%f\n", FLOAT_ARG(0x00013333));
-//	printf("%f %d\n", FLOAT_ARG(0xfffecccd), 123456);
-//#else
-//	sprintf(buf, "%f", FLOAT_ARG(0x00010000));
-//	nemu_assert(strcmp(buf, "1.000000") == 0);
-//	
-//	sprintf(buf, "%f", FLOAT_ARG(0x00013333));
-//	nemu_assert(strcmp(buf, "1.199996") == 0);
-//
-//	sprintf(buf, "%f %d", FLOAT_ARG(0xfffecccd), 123456);
-//	nemu_assert(strcmp(buf, "-1.199996 123456") == 0);
-//#endif
+	init_FLOAT_vfprintf();
+
+#ifdef LINUX_RT
+	printf("%f\n", FLOAT_ARG(0x00010000));
+	printf("%f\n", FLOAT_ARG(0x00013333));
+	printf("%f %d\n", FLOAT_ARG(0xfffecccd), 123456);
+#else
+	sprintf(buf, "%f", FLOAT_ARG(0x00010000));
+	nemu_assert(strcmp(buf, "1.000000") == 0);
+	
+	sprintf(buf, "%f", FLOAT_ARG(0x00013333));
+	nemu_assert(strcmp(buf, "1.199996") == 0);
+
+	sprintf(buf, "%f %d", FLOAT_ARG(0xfffecccd), 123456);
+	nemu_assert(strcmp(buf, "-1.199996 123456") == 0);
+#endif
 
 	return 0;
 }
