050245d2fc195b35caff937f73733cf3ee8af935
tracer-ics2016 <tracer@njuics.org>
2016-11-24 08:56:50 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  08:56:50 up 47 min,  1 user,  load average: 1.02, 1.14, 1.12 bf62fe00c5959f58a71f87586385f7da4de82f

--- nemu/src/cpu/exec/data-mov/lgdt.c
+++ nemu/src/cpu/exec/data-mov/lgdt.c
@@ -4,12 +4,10 @@
 /* By KISS rule, we assume ModR/M byte is 0x15,
  * followed by a disp32, and OperandSize = 32 */
 make_helper(lgdt) {
-	uint8_t modrm = instr_fetch(eip + 2, 1);
-//	assert(modrm == 0x15);
-	Log("%x", modrm);
-	assert(0);
+	uint8_t modrm = instr_fetch(eip + 1, 1);
+	assert(modrm == 0x15);
 
-	swaddr_t addr = instr_fetch(eip + 3, 4);
+	swaddr_t addr = instr_fetch(eip + 2, 4);
 	cpu.gdtr.limit = swaddr_read(addr, 2);
 	cpu.gdtr.base = swaddr_read(addr + 2, 4);
 	print_asm("lgdt 0x%x", addr);
