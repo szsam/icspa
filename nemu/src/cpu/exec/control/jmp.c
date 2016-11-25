34cbadf4065a3e4e244422682a344165a602df13
tracer-ics2016 <tracer@njuics.org>
2016-11-25 17:42:01 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  17:42:01 up  1:23,  1 user,  load average: 1.33, 1.16, 1.15 1cf064593507e29491ed2258979af4fa58698b3a

--- nemu/src/cpu/exec/control/jmp.c
+++ nemu/src/cpu/exec/control/jmp.c
@@ -14,3 +14,12 @@
 
 /* for instruction encoding overloading */
 /* make_helper_v(je_si) */
+
+make_helper(ljmp) {
+	// jmp intersegment, 6-byte immediate address
+	cpu.eip = instr_fetch(eip + 1, 4);
+	cpu.cs.val = instr_fetch(eip + 5, 2);
+	do_not_add_instr_len_to_eip = true;
+	print_asm("ljmp $0x%x,$0x%x", cpu.cs.val, cpu.eip);
+	return 7;
+}
