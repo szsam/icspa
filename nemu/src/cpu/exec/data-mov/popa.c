8a704e21a365192cd0d1ba92ab8fdb8aa6608f74
tracer-ics2016 <tracer@njuics.org>
2016-12-08 19:57:28 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  19:57:28 up 11:52,  1 user,  load average: 1.12, 1.09, 1.12 a9a80e54292e36542ef228edfbfa63d7b62f4098

--- nemu/src/cpu/exec/data-mov/popa.c
+++ nemu/src/cpu/exec/data-mov/popa.c
@@ -5,7 +5,7 @@ make_helper(popa) {
 	// 16 bit version is not implemented
 	assert(!ops_decoded.is_operand_size_16);
 
-	for (int ix = R_EDI; ix <= R_EAX; --ix) {
+	for (int ix = R_EDI; ix >= R_EAX; --ix) {
 		if (ix != R_ESP) {
 			reg_l(ix) = swaddr_read(cpu.esp, 4, R_SS);
 			Log("%s=0x%x", regsl[ix], reg_l(ix));
