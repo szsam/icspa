c9f4126876f00119a306b6abe53d3880561610a7
tracer-ics2016 <tracer@njuics.org>
2016-12-09 21:43:08 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  21:43:08 up  5:21,  1 user,  load average: 1.30, 1.36, 1.28 9e962ab6a31aa45b50aec962c5dbde412f3a65b0

--- nemu/src/cpu/exec/control/ecf.c
+++ nemu/src/cpu/exec/control/ecf.c
@@ -13,13 +13,13 @@ void raise_intr(uint8_t NO, uint32_t eflags, uint16_t cs, uint32_t eip) {
 	// save eflags, cs and eip on stack
 	cpu.esp -= 4;
 	swaddr_write(cpu.esp, 4, eflags, R_SS);
-	Log("eflags=0x%x", eflags);
+	// Log("eflags=0x%x", eflags);
 	cpu.esp -= 4;
 	swaddr_write(cpu.esp, 2, cs, R_SS);
-	Log("CS=0x%x", cs);
+	// Log("CS=0x%x", cs);
 	cpu.esp -= 4;
 	swaddr_write(cpu.esp, 4, eip, R_SS);
-	Log("return address = 0x%x", eip);
+	// Log("return address = 0x%x", eip);
 	
 	// find the gate descriptor
 	lnaddr_t gateDesc_addr = cpu.idtr.base + NO * sizeof(GateDesc);
@@ -55,12 +55,12 @@ make_helper(interrupt) {
 
 make_helper(iret) {
 	cpu.eip = swaddr_read(cpu.esp, 4, R_SS);
-	Log("return address = 0x%x", cpu.eip);
+	// Log("return address = 0x%x", cpu.eip);
 	cpu.cs.val = swaddr_read(cpu.esp + 4, 2, R_SS);
 	cpu.cs.cache.valid = 0;
-	Log("CS=0x%x", cpu.cs.val);
+	// Log("CS=0x%x", cpu.cs.val);
 	cpu.EFLAGS = swaddr_read(cpu.esp + 8, 4, R_SS);
-	Log("eflags=0x%x", cpu.EFLAGS);
+	// Log("eflags=0x%x", cpu.EFLAGS);
 	cpu.esp += 12;
 
 	do_not_add_instr_len_to_eip = true;
