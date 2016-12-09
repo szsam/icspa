cf4255cfc46533ab54f856e3df3f61e495fe66ad
tracer-ics2016 <tracer@njuics.org>
2016-12-09 20:03:16 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  20:03:16 up  3:41,  1 user,  load average: 1.05, 1.12, 1.13 bf1ba545509a22906985c68326685e5f3a5ee482

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -89,6 +89,7 @@ void cpu_exec(volatile uint32_t n) {
 		device_update();
 
 		if(cpu.INTR & cpu.IF) {
+			Log("interrupt");
 			uint32_t intr_no = i8259_query_intr();
 			i8259_ack_intr();
 			raise_intr(intr_no);
