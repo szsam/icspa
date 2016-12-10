12b139ef7d3c6f824f87ec3243e3eedc4b401f4b
tracer-ics2016 <tracer@njuics.org>
2016-12-10 11:21:11 +0800

    >  compile NEMU 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  11:21:11 up 23 min,  1 user,  load average: 1.13, 1.14, 0.96 399cbe1020dbba1096af513045406e546473b336

--- nemu/src/cpu/exec/system/hlt.c
+++ nemu/src/cpu/exec/system/hlt.c
@@ -1,6 +1,18 @@
 #include "cpu/helper.h"
+#include "device/i8259.h"
+
+void device_update();
+void raise_intr(uint8_t, uint32_t, uint16_t, uint32_t);
 
 make_helper(hlt) {
-	while (!(cpu.INTR & cpu.IF));
+	while (!(cpu.INTR & cpu.IF)) {
+		device_update();
+	}
+
+	uint32_t intr_no = i8259_query_intr();
+	i8259_ack_intr();
+	raise_intr(intr_no, cpu.EFLAGS, cpu.cs.val, cpu.eip);
+
+	panic("control should never reaches here");
 	return 1;
 }
