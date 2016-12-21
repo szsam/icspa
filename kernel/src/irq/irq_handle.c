0b1fb4620f18eb9e15360bccaa1ac866881a707b
tracer-ics2016 <tracer@njuics.org>
2016-12-21 16:41:19 +0800

    >  compile kernel 151220091 shen Linux Lenovo-G50-80m 4.4.8-040408-generic #201604200335 SMP Wed Apr 20 07:51:11 UTC 2016 i686 i686 i686 GNU/Linux  16:41:19 up 41 min,  1 user,  load average: 1.13, 1.11, 1.13 a0af9676cf1aba36ee86eda5f6631068267de7f3

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -38,8 +38,7 @@ void irq_handle(TrapFrame *tf) {
 	int irq = tf->irq;
 
 	if (irq < 0) {
-		// panic("Unhandled exception!");
-		panic("Unexpected exception #%d at eip = %x", irq, tf->eip);
+		panic("Unhandled exception!");
 	} else if (irq == 0x80) {
 		do_syscall(tf);
 	} else if (irq < 1000) {
